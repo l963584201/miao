85aa7936614ebee6227cf955b0ccd78a344e6f93
nwj <96358401@qq.com>
2020-04-28 15:53:15 +0800

    work

---
+++ l963584201-lodash.js
@@ -0,0 +1,22 @@
+var l963584201 = {
+	isNull: function (val)
+	{
+		if (val === null)
+		{
+			return true
+		} else
+		{
+			return false
+		}
+	},
+	isNaN: function (val)
+	{
+		if (val !== val)
+		{
+			return true
+		} else
+		{
+			return false
+		}
+	}
+}
