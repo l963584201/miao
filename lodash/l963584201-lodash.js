6a449adec7d413c22966bf5a03ddf09676e954f2
nwj <96358401@qq.com>
2020-07-19 16:50:50 +0800

    work

--- lodash/l963584201-lodash.js
+++ lodash/l963584201-lodash.js
@@ -32,36 +32,28 @@ var l963584201 = {
 		}
 		return array.filter((x) => !arrays.includes(x))
 	},
-	drop: function (array, n=1)
-	{
-		for (let i = 1; i <= n; i++)
-		{
+	drop: function (array, n=1){
+		for (let i = 1; i <= n; i++){
 			array.shift()
 		}
 		return array
 	},
-	fill: function (array,value,start = 0,end = array.length)
-	{
-		for (let i = start; i < end; i++)
-		{
+	fill: function (array,value,start = 0,end = array.length){
+		for (let i = start; i < end; i++){
 			array[i] = value
 		}
 		return array
 	},
-	dropRight: function (array,n = 1)
-	{
-		if (n <= array.length)
-		{for (i = 0; i < n; i++)
-			{
+	dropRight: function (array,n = 1){
+		if (n <= array.length){
+			for (i = 0; i < n; i++){
 				array.pop()
 		}
 			return array
-		} else if (n = 0)
-		{
+		} else if (n = 0){
 			return array
 		}
-		else
-		{
+		else{
 			return []
 		}
 	},
